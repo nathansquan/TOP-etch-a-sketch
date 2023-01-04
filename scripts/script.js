b57b9c23bc944b2c5ab4d8a3c50e9c82f9fe15f3
Nathan Quan <nathansquan@gmail.com>
2023-01-03 22:21:34 -0500

    Create webpage with a 16x16 grid of square divs

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+// Want 16 x 16 grid
+// create 16 containers with 16 grid cells each
+// select container div
+
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+    const colContainer = document.createElement('div');
+    colContainer.setAttribute('class', 'col-container');
+    colContainer.setAttribute('id', `col-container-${i}`);
+    container.appendChild(colContainer);
+
+    for (let j = 0; j < 16; j++) {
+        const gridCell = document.createElement('div');
+        gridCell.setAttribute('class', 'grid-cell');
+        gridCell.setAttribute('id', `grid-cell-${j}`);
+        colContainer.appendChild(gridCell);
+    }
+}
+
